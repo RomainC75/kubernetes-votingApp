2070efbcc75be5062ce5234b716e944a3b6f0401
Romain CHENARD <rom@Romains-MacBook-Air.local>
2025-01-10 21:52:05 +0100

    feat: 🎸 job gets list of votes

--- job/main.go
+++ job/main.go
@@ -1,13 +1,30 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"os"
+	db "shared/postgres/sqlc"
+
+	"github.com/sirupsen/logrus"
 )
 
 func main() {
+	db.Connect()
+
 	values := os.Environ()
 	for index, v := range values {
 		fmt.Printf("%d : %s\n", index, v)
 	}
+
+	conn := db.GetConnection()
+	ctx := context.Background()
+
+	votes, err := (*conn).ListVotes(ctx)
+	if err != nil {
+		logrus.Error("=> error trying to get list of votes", err.Error())
+	}
+
+	fmt.Println("->  ", votes)
+
 }
